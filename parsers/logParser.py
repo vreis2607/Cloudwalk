efd337d77e86920a735a29b9ed3143140ab9ef75
Vitor dos Reis <vreis2607@gmail.com>
2022-06-06 16:06:19 -0300

    Do function to parse

--- parsers/logParser.py
+++ parsers/logParser.py
@@ -5,3 +5,32 @@ start_regex = re.compile(r".*InitGame:.*")
 
 # Regex match kill lines of log
 kill_regex = re.compile(r".*Kill:.*:(.*).*killed(.*)by(.*)")
+
+# Function to parse games, kills, players
+def parse_game_kills(logfile, show_weapon=False):
+    """Get logfile and parse results into OrderedDict"""
+    game_match_count = 1
+    key_map = "game_{}"
+    parsed_game_matches = OrderedDict()
+    with open(logfile, "r", encoding="utf-8") as fp:
+        for line in fp.readlines():
+            if start_regex.match(line):
+                key = key_map.format(game_match_count)
+                if show_weapon:
+                    parsed_game_matches[key] = {
+                        "total_kills": 0,
+                        "players": [],
+                        "kills": {},
+                        "kills_by_means": {}
+                    }
+                else:
+                    parsed_game_matches[key] = {
+                        "total_kills": 0,
+                        "players": [],
+                        "kills": {},
+                    }
+                game_match_count += 1
+
+            if kill_regex.match(line):
+                parse_kill_line(line, parsed_game_matches[key], show_weapon)
+    return parsed_game_matches
